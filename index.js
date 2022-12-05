d26cb264bed3d3997f032219cff26bfd8880672f
Ashish Patel <ashishpatel3946@gmail.com>
2022-12-05 14:11:15 +0530

    ready for deploy

--- index.js
+++ index.js
@@ -92,5 +92,19 @@ app.delete("/deletepic", (req, res) => {
         }
        
         
-    });
-app.listen(8000, () => { console.log("server is connected at 8000") });
+});
+const port = process.env.PORT || 8000;
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("Client/build"));
+      app.get("*", (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname + "/Client/build/index.html"),
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      });
+    }
+app.listen(port, () => { console.log("server is connected at 8000") });
