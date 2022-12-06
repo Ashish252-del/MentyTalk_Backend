f3995fab3eb9d851151cd336777da8ce083d873b
Ashish Patel <ashishpatel3946@gmail.com>
2022-12-06 18:09:53 +0530

    new

--- index.js
+++ index.js
@@ -94,17 +94,17 @@ app.delete("/deletepic", (req, res) => {
         
 });
 const port = process.env.PORT || 8000;
-    // if (process.env.NODE_ENV !== "production") {
-    //   app.use(express.static("Client/build"));
-    //   app.get("*", (req, res) => {
-    //     res.sendFile(
-    //       path.resolve(__dirname + "/Client/build/index.html"),
-    //       function (err) {
-    //         if (err) {
-    //           console.log(err);
-    //         }
-    //       }
-    //     );
-    //   });
-    // }
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("Client/build"));
+      app.get("*", (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname + "/Client/build/index.html"),
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      });
+    }
 app.listen(port, () => { console.log("server is connected at 8000") });
